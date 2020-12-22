71333f2697b7959de77d59cd9cb4342d1e60cc98
Nazire ALIÇ <58981496+nzralc@users.noreply.github.com>
2020-12-22 15:58:26 +0300

    Create Homework1-nzralc.py
    Global AI Hub- Python ödev 1 yapıldı.

---
+++ Homework1-nzralc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+isim=input("Lütfen isminizi giriniz: ")
+soyisim=input("\n Lütfen soyisminizi giriniz: ")
+tel=input("\n Telefon numaranız:")
+email=input("\n Mail adresiniz:")
+sehir=input("\n Yaşadığınız şehir:")
+print('\n\n Bu ödevi yapan kişinin adı: {ad}'.format(ad=isim),
+      " \n Telefon numarası:",tel,
+      " \n Email adresi:",email,
+      " \n Yaşadığınız şehir:",sehir,"dır.")
+"Adım: {} , Soyadım: {} ve Yaşadığım şehir: {} ".format(isim, soyisim,sehir)
+
+
+# In[ ]:
+
+
+
+
